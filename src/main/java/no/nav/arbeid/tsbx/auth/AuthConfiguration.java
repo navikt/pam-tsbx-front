06644cf6383b32131f4c2792a1cd397559bdb871
Øyvind Stegard <oyvind.stegard@nav.no>
2021-11-18 18:07:08 +0100

    Debug: log issuer well known metadata at app init

--- src/main/java/no/nav/arbeid/tsbx/auth/AuthConfiguration.java
+++ src/main/java/no/nav/arbeid/tsbx/auth/AuthConfiguration.java
@@ -5,6 +5,8 @@ import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.openid.connect.sdk.op.OIDCProviderConfigurationRequest;
 import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,17 @@ import java.net.MalformedURLException;
 @Configuration
 public class AuthConfiguration {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AuthConfiguration.class);
+
     @Bean
     public OIDCProviderMetadata idPortenMetadata(IdPortenConfigurationProperties idPortenConfig) throws IOException, ParseException {
         Issuer issuer = new Issuer(idPortenConfig.wellKnownUrl());
         HTTPResponse response = new OIDCProviderConfigurationRequest(issuer).toHTTPRequest().send();
+
+        // todo log well known metadata response
+        LOG.info("OIDC issuer well known metadata:");
+        LOG.info(response.getContent());
+
         return OIDCProviderMetadata.parse(response.getContent());
     }
 
