2617a69e0fddf4705a1f861910e33517cf9b636d
Øyvind Stegard <oyvind.stegard@nav.no>
2021-11-25 19:36:54 +0100

    Enable caching of tokens obtained through exchange

--- src/main/java/no/nav/arbeid/tsbx/messages/MessagesApiClientConfiguration.java
+++ src/main/java/no/nav/arbeid/tsbx/messages/MessagesApiClientConfiguration.java
@@ -21,7 +21,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @Configuration
-@EnableOAuth2Client
+@EnableOAuth2Client(cacheEnabled = true)
 public class MessagesApiClientConfiguration {
 
     @Bean
