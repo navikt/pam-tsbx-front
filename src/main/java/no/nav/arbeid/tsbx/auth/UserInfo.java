67e39977aceed792b370a8e51a918937a11ad8ef
Øyvind Stegard <oyvind.stegard@nav.no>
2021-11-25 10:24:59 +0100

    Map ID-porten subject identifiers to fake generated names

--- src/main/java/no/nav/arbeid/tsbx/auth/UserInfo.java
+++ src/main/java/no/nav/arbeid/tsbx/auth/UserInfo.java
@@ -1,3 +1,57 @@
 package no.nav.arbeid.tsbx.auth;
 
-public record UserInfo(String name, String pid) {}
+import java.util.Random;
+
+public record UserInfo(String name, String pid) {
+
+    public String name() {
+        if (name.endsWith("=")) {
+            // Likely ID-porten subject identifier, convert to a fake name that consistently follows from it.
+            return generateNameFromOpaqueSubjectIdentifier(name);
+        }
+        return name;
+    }
+
+    /**
+     * ID-porten does not provide actual names in ID-tokens, så make up some names here, based on opaque subject identifier.
+     * In a real app, this data might come from PDL.
+     */
+    private static String generateNameFromOpaqueSubjectIdentifier(String id) {
+        final Random random = new Random(id.hashCode());
+        final String[] firstNames = {
+                "Nora", "Jakob",
+                "Emma", "Emil",
+                "Ella", "Noah",
+                "Maja", "Oliver",
+                "Olivia", "Filip",
+                "Emilie", "William",
+                "Sofie", "Lukas",
+                "Leah", "Liam",
+                "Sofia", "Henrik",
+                "Ingrid", "Oskar"
+        };
+        final String[] lastNames = {
+                "Hansen",
+                "Johansen",
+                "Olsen",
+                "Larsen",
+                "Andersen",
+                "Pedersen",
+                "Nilsen",
+                "Kristiansen",
+                "Jensen",
+                "Karlsen",
+                "Johnsen",
+                "Pettersen",
+                "Eriksen",
+                "Berg",
+                "Haugen",
+                "Hagen",
+                "Johannessen",
+                "Andreassen"
+        };
+
+        return firstNames[random.nextInt(firstNames.length)] + " " + lastNames[random.nextInt(lastNames.length)];
+    }
+
+}
